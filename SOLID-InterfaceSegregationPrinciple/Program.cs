a4e4f40badae02d9e6be40022bea9bc7eb8c5295
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-12 13:22:46 +0300

    Interface Segregation Principle

---
+++ SOLID-InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace SOLID_InterfaceSegregationPrinciple
+{
+    public class Document { }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException(nameof(Scan));
+        }
+
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException(nameof(Fax));
+        }
+    }
+
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public interface IFax
+    {
+        void Fax(Document d);
+    }
+
+    public class PhotoCopier : IScanner, IPrinter
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionMachine : IScanner, IPrinter // ....
+    {
+    }
+
+    public class MultiFunctionMachine : IMultiFunctionMachine
+    {
+        IPrinter printer;
+        IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+            // decorator pattern
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- SOLID-SingleResponsibility/Program.cs
+++ SOLID-SingleResponsibility/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using static System.Console;
 
-namespace SingleResponsibility
+namespace SOLID_SingleResponsibility
 {
     public class Journal
     {
