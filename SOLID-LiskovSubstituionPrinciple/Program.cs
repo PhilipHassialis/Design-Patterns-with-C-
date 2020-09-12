8c20218547317046065a0c217f9c052f7a966c62
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-12 13:03:45 +0300

    Liskov substitution

---
+++ SOLID-LiskovSubstituionPrinciple/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SOLID_LiskovSubstituionPrinciple
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+        public Rectangle() { }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width} - {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+        public override int Height
+        {
+            set { base.Height = base.Width = value; }
+        }
+
+
+    }
+
+    class Program
+    {
+        static public int Area(Rectangle rc) => rc.Width * rc.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 9);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 6;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+
+        }
+    }
+}
