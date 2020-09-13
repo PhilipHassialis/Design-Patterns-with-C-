05717b839b9e5e45bf513f2138486dce3269760a
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-14 00:04:43 +0300

    Asynchronous Factory

---
+++ Design Patterns/Factory/AsynchronousFactoryMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousFactoryMethod
+{
+
+    public class Foo
+    {
+        private Foo() { }
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var f = await Foo.CreateAsync();
+        }
+    }
+}
