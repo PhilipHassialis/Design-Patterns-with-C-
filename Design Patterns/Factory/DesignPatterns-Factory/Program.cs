9a65a9f99a41b94ed53e0cce6ec69b46d6856937
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-14 00:15:28 +0300

    Abstract Factory

--- Design Patterns/Factory/DesignPatterns-Factory/Program.cs
+++ Design Patterns/Factory/DesignPatterns-Factory/Program.cs
@@ -2,30 +2,27 @@
 
 namespace DesignPatterns_Factory
 {
-    public enum CoordinateSystem
-    {
-        Cartesian, Polar
-    }
 
-    public class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
 
     public class Point
     {
         private double x, y;
 
-        // factory method design pattern
+        // public static Point Origin => new Point(0, 0); worse approach
+        public static Point Origin = new Point(0, 0); // better approach
 
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
 
 
         public override string ToString()
@@ -33,7 +30,7 @@ namespace DesignPatterns_Factory
             return $"x:{x} y:{y}";
         }
 
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
@@ -64,7 +61,7 @@ namespace DesignPatterns_Factory
     {
         static void Main(string[] args)
         {
-            Point p = PointFactory.NewPolarPoint(1, Math.PI / 2);
+            Point p = Point.Factory.NewPolarPoint(1, Math.PI / 2);
             Console.WriteLine(p);
         }
     }
