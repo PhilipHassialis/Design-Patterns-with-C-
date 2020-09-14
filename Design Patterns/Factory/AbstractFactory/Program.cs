f251fc327f03057f93f81373c2f2c98d66aadfe9
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-14 07:34:23 +0300

    Abstract Factory

---
+++ Design Patterns/Factory/AbstractFactory/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace AbstractFactory
+{
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("Consuming Tea");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine("Preparing Tea");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine("Preparing Cofeee");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink
+        {
+            Tea, Coffee
+        }
+        private Dictionary<AvailableDrink, IHotDrinkFactory> Factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            {
+                var factory = (IHotDrinkFactory)Activator.CreateInstance(
+                    Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
+                );
+                Factories.Add(drink, factory);
+            }
+        }
+
+        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        {
+            return Factories[drink].Prepare(amount);
+        }
+    }
+
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("Consuming Coffee");
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 2);
+            drink.Consume();
+        }
+    }
+}
