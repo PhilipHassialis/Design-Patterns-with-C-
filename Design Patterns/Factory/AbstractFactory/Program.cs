1daec69a1387e349b47e0071748f07899c4fe38d
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-14 07:50:13 +0300

    Abstract Factory and OCP

--- Design Patterns/Factory/AbstractFactory/Program.cs
+++ Design Patterns/Factory/AbstractFactory/Program.cs
@@ -16,6 +16,13 @@ namespace AbstractFactory
         }
     }
 
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("Consuming Coffee");
+        }
+    }
     public interface IHotDrinkFactory
     {
         IHotDrink Prepare(int amount);
@@ -41,37 +48,73 @@ namespace AbstractFactory
 
     public class HotDrinkMachine
     {
-        public enum AvailableDrink
-        {
-            Tea, Coffee
-        }
-        private Dictionary<AvailableDrink, IHotDrinkFactory> Factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+        // the following violates OpenClosed Principle
+        // ====================================================
+
+        // public enum AvailableDrink
+        // {
+        //     Tea, Coffee
+        // }
+        // private Dictionary<AvailableDrink, IHotDrinkFactory> Factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        // public HotDrinkMachine()
+        // {
+        //     foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //     {
+        //         var factory = (IHotDrinkFactory)Activator.CreateInstance(
+        //             Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
+        //         );
+        //         Factories.Add(drink, factory);
+        //     }
+        // }
+
+        // public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        // {
+        //     return Factories[drink].Prepare(amount);
+        // }
+
+        private List<Tuple<string, IHotDrinkFactory>> factories = new List<Tuple<string, IHotDrinkFactory>>();
 
         public HotDrinkMachine()
         {
-            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            foreach (var machineType in typeof(HotDrinkMachine).Assembly.GetTypes())
             {
-                var factory = (IHotDrinkFactory)Activator.CreateInstance(
-                    Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
-                );
-                Factories.Add(drink, factory);
+                if (typeof(IHotDrinkFactory).IsAssignableFrom(machineType) && !machineType.IsInterface)
+                {
+                    factories.Add(Tuple.Create(
+                        machineType.Name.Replace("Factory", String.Empty),
+                        (IHotDrinkFactory)Activator.CreateInstance(machineType)
+                    ));
+                }
             }
         }
 
-        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        public IHotDrink MakeDrink()
         {
-            return Factories[drink].Prepare(amount);
+            WriteLine("Available Drinks:");
+            for (int i = 0; i < factories.Count; i++)
+            {
+                Tuple<string, IHotDrinkFactory> tuple = (Tuple<string, IHotDrinkFactory>)factories[i];
+                WriteLine($"{i}:{tuple.Item1}");
+            }
+            while (true)
+            {
+                string s;
+                if ((s = Console.ReadLine()) != null && int.TryParse(s, out int i) && i >= 0 && i < factories.Count)
+                {
+                    Write("Specify amount: ");
+                    s = Console.ReadLine();
+                    if (s != null && int.TryParse(s, out int amount) && amount > 0)
+                    {
+                        return factories[i].Item2.Prepare(amount);
+                    }
+                }
+                WriteLine("Incorrect input, try again");
+            }
         }
     }
 
 
-    internal class Coffee : IHotDrink
-    {
-        public void Consume()
-        {
-            WriteLine("Consuming Coffee");
-        }
-    }
 
 
 
@@ -81,7 +124,8 @@ namespace AbstractFactory
         static void Main(string[] args)
         {
             var machine = new HotDrinkMachine();
-            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 2);
+            // var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 2);
+            var drink = machine.MakeDrink();
             drink.Consume();
         }
     }
