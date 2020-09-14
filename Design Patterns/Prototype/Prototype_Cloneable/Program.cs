67a69d1f1b9fa23c5b7b3cd9ff0fa35c45a5f256
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-14 21:20:44 +0300

    Copy through Serialization

--- Design Patterns/Prototype/Prototype_Cloneable/Program.cs
+++ Design Patterns/Prototype/Prototype_Cloneable/Program.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
 namespace Prototype_Cloneable
 {
-    public interface IPrototype<T>
+    public static class ExtensionMethods
     {
-        T DeepCopy();
+        public static T DeepCopy<T>(this T self)
+        {
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, self);
+            stream.Seek(0, SeekOrigin.Begin);
+            object copy = formatter.Deserialize(stream);
+            stream.Close();
+            return (T)copy;
+        }
+
+        public static T DeepCopyXml<T>(this T self)
+        {
+            using var ms = new MemoryStream();
+            var s = new XmlSerializer(typeof(T));
+            s.Serialize(ms, self);
+            ms.Position = 0;
+            return (T)s.Deserialize(ms);
+
+        }
     }
 
-    public class Person : IPrototype<Person>
+    // public interface IPrototype<T>
+    // {
+    //     T DeepCopy();
+    // }
+
+
+    // [Serializable]
+    public class Person //: IPrototype<Person>
     {
         public string[] Names;
         public Address Address;
@@ -24,20 +54,31 @@ namespace Prototype_Cloneable
             Address = new Address(other.Address);
         }
 
+        public Person()
+        {
+        }
+
         public override string ToString()
         {
             return $"{nameof(Names)}: {string.Join(" ", Names)} {nameof(Address)}: {Address}";
         }
 
-        public Person DeepCopy()
-        {
-            return new Person(Names, Address.DeepCopy());
-        }
+        // public Person DeepCopy()
+        // {
+        //     return new Person(Names, Address.DeepCopy());
+        // }
     }
-    public class Address : IPrototype<Address>
+
+
+    // [Serializable]
+    public class Address // : IPrototype<Address>
     {
         public string StreetName;
         public int HouseNumber;
+
+        public Address()
+        {
+        }
 
         public Address(Address other)
         {
@@ -51,10 +92,11 @@ namespace Prototype_Cloneable
             HouseNumber = houseNumber;
         }
 
-        public Address DeepCopy()
-        {
-            return new Address(StreetName, HouseNumber);
-        }
+
+        // public Address DeepCopy()
+        // {
+        //     return new Address(StreetName, HouseNumber);
+        // }
 
         public override string ToString()
         {
@@ -77,8 +119,10 @@ namespace Prototype_Cloneable
             );
 
             // var Jane = new Person(John);
-            var Jane = John.DeepCopy();
+            // var Jane = John.DeepCopy();
+            var Jane = John.DeepCopyXml();
             Jane.Address.HouseNumber = 10;
+            Jane.Names[0] = "Jane";
 
             Console.WriteLine(John);
             Console.WriteLine(Jane);
