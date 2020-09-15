aeeefa3d1f131b786547f8cb0a8bee067411d6ff
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-15 22:07:35 +0300

    MonoState

---
+++ Design Patterns/Singleton/Monostate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Monostate
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public int Age { get => age; set => age = value; }
+        public string Name { get => name; set => name = value; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new CEO() { Name = "Philip", Age = 45 };
+            var ceo2 = new CEO();
+            Console.WriteLine(ceo2);
+
+        }
+    }
+}
