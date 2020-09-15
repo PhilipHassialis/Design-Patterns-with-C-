e3b398f76ba67cf33773a590fa7f91b437ab6f4c
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-15 08:08:18 +0300

    Singleton Implemenation

---
+++ Design Patterns/Singleton/SingletonImplementation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MoreLinq;
+
+namespace SingletonImplementation
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initializing db");
+            capitals = File.ReadAllLines("capitals.txt")
+                .Batch(2)
+                .ToDictionary(list => list.ElementAt(0).Trim(), list => int.Parse(list.ElementAt(1)));
+
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static readonly Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase());
+        public static SingletonDatabase Instance => instance.Value;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDatabase.Instance;
+            Console.WriteLine($"Tokyo has population of : {db.GetPopulation("Tokyo")}");
+        }
+    }
+}
