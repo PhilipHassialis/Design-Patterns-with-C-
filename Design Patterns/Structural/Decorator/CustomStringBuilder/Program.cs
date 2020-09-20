b762cbf8c91fe034c9e69528a4d3a065b1197e08
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-20 15:51:41 +0300

    Adapter - Custom StringBuuilder

---
+++ Design Patterns/Structural/Decorator/CustomStringBuilder/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace CustomStringBuilder
+{
+    class Program
+    {
+        public class CodeBuilder
+        {
+            private StringBuilder builder = new StringBuilder();
+
+            // create declarative methods for the relevant builder methods. E.g.
+            public CodeBuilder Clear()
+            {
+                builder.Clear();
+                return this;
+            }
+
+            public CodeBuilder(string val)
+            {
+                builder = new StringBuilder(val);
+            }
+
+            public CodeBuilder()
+            {
+                
+            }
+
+            public CodeBuilder Append(string val)
+            {
+                builder.Append(val);
+                return this;
+            }
+
+            public CodeBuilder AppendLine(string val)
+            {
+                builder.AppendLine(val);
+                return this;
+            }
+
+            // .....
+
+            public override string ToString()
+            {
+                return builder.ToString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CodeBuilder cb = new CodeBuilder();
+            cb.AppendLine("class MyClass")
+                .AppendLine("{")
+                .AppendLine("}");
+
+            Console.WriteLine(cb);
+        }
+    }
+}
