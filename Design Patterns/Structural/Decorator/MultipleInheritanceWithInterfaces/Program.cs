3e605c963d300ed4a57c2bcdf7fd79c257063480
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-20 16:04:05 +0300

    Multiple Inheritance with Interfaces

---
+++ Design Patterns/Structural/Decorator/MultipleInheritanceWithInterfaces/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MultipleInheritanceWithInterfaces
+{
+    public interface IBird
+    {
+        int Weight { get; set; }
+
+        void Fly();
+    }
+
+    public class Bird : IBird
+    {
+        public int Weight { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine($"Bird flying with weight {Weight}");
+        }
+    }
+
+    public interface ILizard
+    {
+        int Weight { get; set; }
+
+        void Crawl();
+    }
+
+    public class Lizard : ILizard
+    {
+        public int Weight { get; set; }
+        public void Crawl()
+        {
+            Console.WriteLine($"Lizard crawling with weight {Weight}");
+        }
+    }
+
+    public class Dragon : IBird, ILizard
+    {
+        private Bird bird = new Bird();
+        private Lizard lizard = new Lizard();
+        private int weight;
+
+        public void Crawl() { lizard.Crawl(); }
+        public void Fly() { bird.Fly(); }
+        public int Weight { 
+            get => weight;
+            set { bird.Weight = lizard.Weight = weight = value;  }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dragon dr = new Dragon();
+            dr.Weight = 300;
+            dr.Fly();
+            dr.Crawl();
+        }
+    }
+}
