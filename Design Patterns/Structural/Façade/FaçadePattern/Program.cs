579448493031a2e13c2e6932a1030a193bcf9700
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-22 21:56:37 +0300

    Facade Pattern & Coding Exercise

---
+++ Design Patterns/Structural/Façade/FaçadePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FaçadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Façade!");
+        }
+    }
+}
