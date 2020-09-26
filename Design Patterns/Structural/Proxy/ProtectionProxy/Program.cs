8a60d2ee5be8b3df758042f3c2780a30625b10ea
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-26 15:35:22 +0300

    Proxy - Protection Proxy

---
+++ Design Patterns/Structural/Proxy/ProtectionProxy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ProtectionProxy
+{
+ 
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is driven");
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Driver driver;
+        private Car car = new Car();
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 18) car.Drive(); else Console.WriteLine("too young");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver() { Age = 12 });
+            car.Drive();
+        }
+    }
+}
