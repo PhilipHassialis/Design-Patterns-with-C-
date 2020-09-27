e7f48a07d5de90b9718be91d54428eb1b628df7d
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-27 16:02:58 +0300

    ViewModel & Proxy Coding Exercise

---
+++ Design Patterns/Structural/Proxy/ViewModel/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    // model
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    // view = ui
+
+    // viewmodel
+    public class PersonViewModel:INotifyPropertyChanged
+    {
+        private readonly Person person;
+
+        public PersonViewModel(Person person)
+        {
+            this.person = person;
+        }
+
+        public string FirstName
+        {
+            get => person.FirstName;
+            set
+            {
+                if (person.FirstName == value) return;
+                person.FirstName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(FirstName)));
+            }
+        }
+
+        public string LastName
+        {
+            get => person.LastName;
+            set
+            {
+                if (person.LastName == value) return;
+                person.LastName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(LastName)));
+            }
+
+        }
+
+        public string FullName
+        {
+            get => $"{FirstName} {LastName}".Trim();
+            set
+            {
+                if (value == null) { FirstName = LastName = null; return; }
+                var items = value.Split();
+                if (items.Length > 0) FirstName = items[0];
+                if (items.Length > 1) LastName = items[1];
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public override string ToString()
+        {
+            return $"PersonViewModel for Person {LastName} {FirstName}";
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pvm = new PersonViewModel(new Person() { FirstName = "Philip", LastName = "Hassialis" });
+            pvm.PropertyChanged += (obj, args) =>
+            {
+                Console.WriteLine(obj.ToString());
+            };
+            pvm.FirstName = "Philip - Alexander";
+        }
+    }
+}
---
+++ Exercises/ProxyCodingExercise/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ProxyCodingExercise
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Drink()
+        {
+            return "drinking";
+        }
+
+        public string Drive()
+        {
+            return "driving";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "driving while drunk";
+        }
+    }
+
+    public class ResponsiblePerson
+    {
+        private readonly Person person;
+        public ResponsiblePerson(Person person)
+        {
+            this.person = person;
+        }
+
+        public int Age
+        {
+            get { return person.Age; }
+            set { person.Age = value; }
+        }
+
+        public string Drink()
+        {
+            return Age<=18?"too young":person.Drink();
+        }
+
+        public string Drive()
+        {
+            return Age <= 16 ? "too young" : person.Drive();
+        }
+
+        public string DrinkAndDrive() => "dead";
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
