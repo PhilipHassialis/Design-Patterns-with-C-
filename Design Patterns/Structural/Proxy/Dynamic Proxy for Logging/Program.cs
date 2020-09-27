e9a2f911fcdcd933f433924491e30adbdbed8ba8
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-27 12:51:45 +0300

    Solution Restructuring for proper folder placement

---
+++ Design Patterns/Structural/Proxy/Dynamic Proxy for Logging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dynamic_Proxy_for_Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
