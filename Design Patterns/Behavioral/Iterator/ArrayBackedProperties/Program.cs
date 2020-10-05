37ff40f8a2e071737c8942a5e1a77e7ef327539a
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-10-06 00:28:04 +0300

    Array backed properties

---
+++ Design Patterns/Behavioral/Iterator/ArrayBackedProperties/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayBackedProperties
+{
+    public class Creature:IEnumerable<int>
+    {
+        private int[] stats = new int[3];
+        private const int strength = 0;
+
+        public int Str { get { return stats[strength]; } set { stats[strength] = value; } }
+        public int Agi { get => stats[1]; set => stats[1] = value; }
+        public int Int { get => stats[2]; set => stats[2] = value; }
+        public double AverageStat => stats.Average();
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return stats.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        //{
+        //    get { return (Str + Agi + Int) / 3.0; }
+        //}
+
+        public int this[int index]
+        {
+            get => stats[index];
+            set => stats[index] = value;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
