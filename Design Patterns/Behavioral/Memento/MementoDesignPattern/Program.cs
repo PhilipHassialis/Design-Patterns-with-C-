4fbf02e17eed1a473dd957c0b3b3149fff258345
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-10-10 12:03:26 +0300

    Memento

---
+++ Design Patterns/Behavioral/Memento/MementoDesignPattern/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MementoDesignPattern
+{
+    public class Memento
+    {
+        public int Balance { get; }
+
+        public Memento (int balance)
+        {
+            Balance = balance;
+        }
+    }
+
+    public class BankAccount
+    {
+        private int balance;
+
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+        }
+
+        public Memento Deposit(int amount)
+        {
+            balance += amount;
+            return new Memento(balance);
+        }
+
+        public void Restore(Memento m)
+        {
+            balance = m.Balance;
+        }
+
+        public override string ToString()
+        {
+            return $"Balance: {balance}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount(100);
+            var m1 = ba.Deposit(50);
+            var m2 = ba.Deposit(75);
+            Console.WriteLine(ba);
+
+            ba.Restore(m1);
+            Console.WriteLine(ba);
+
+            ba.Restore(m2);
+            Console.WriteLine(ba);
+        }
+    }
+}
