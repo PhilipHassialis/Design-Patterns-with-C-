0f7cd3df2a6f558f68c90b37f4870f3f22cb5b5c
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-10-13 00:54:07 +0300

    Weak Event (.NET Framework only)

---
+++ Design Patterns/Behavioral/Observer/WeakEventPattern/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace WeakEventPattern
+{
+    public class Button
+    {
+        public event EventHandler Clicked;
+        public void Fire()
+        {
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Window
+    {
+        public Window(Button button)
+        {
+            button.Clicked += ButtonClicked;
+            //WeakEventManager<Button, EventArgs>.AddHandler(button, "Clicked", ButtonClicked); .NET Framework only
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked (Window Handler)");
+        }
+
+        ~Window()
+        {
+            Console.WriteLine("Window finalised");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var btn = new Button();
+            var window = new Window(btn);
+            var windowRef = new WeakReference(window);
+            btn.Fire();
+            Console.WriteLine("Setting window to null");
+            window = null;
+            FireGC();
+            Console.WriteLine($"Is the window alive after GC {windowRef.IsAlive}");
+        }
+
+        private static void FireGC()
+        {
+            Console.WriteLine("Starting GC");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            Console.WriteLine("GC is done");
+        }
+    }
+}
