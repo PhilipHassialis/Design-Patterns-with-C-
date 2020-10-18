fc049539b76ad646f32c5b19049d2f7ad91b3d7f
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-10-18 12:14:18 +0300

    Switch Expression State Machine

---
+++ Design Patterns/Behavioral/State/Switch Expression State Machine/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Switch_Expression_State_Machine
+{
+    enum Chest
+    {
+        Open, 
+        Locked,
+        Closed
+    }
+
+    enum Action
+    {
+        Open,
+        Close
+    }
+
+
+
+    class Program
+    {
+        static Chest Manipulate(Chest chest, Action action, bool haveKey) =>
+            (chest, action, haveKey) switch
+            {
+                (Chest.Locked, Action.Open, true) => Chest.Open,
+                (Chest.Closed, Action.Open, _) => Chest.Open,
+                (Chest.Open, Action.Close, true) => Chest.Locked,
+                (Chest.Open, Action.Close, false) => Chest.Closed,
+                _ => chest
+                
+            };
+
+
+
+
+        static void Main(string[] args)
+        {
+            var chest = Chest.Locked;
+            Console.WriteLine($"Chest is {chest}");
+            chest = Manipulate(chest, Action.Open, true);
+            Console.WriteLine($"Chest is {chest}");
+            chest = Manipulate(chest, Action.Close, false);
+            Console.WriteLine($"Chest is {chest}");
+            chest = Manipulate(chest, Action.Close, false);
+            Console.WriteLine($"Chest is {chest}");
+
+        }
+    }
+}
