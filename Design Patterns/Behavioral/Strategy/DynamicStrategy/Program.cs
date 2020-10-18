cd4616105f4f1f48609e7c5b3e1d0fee19fe8fa7
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-10-18 15:51:43 +0300

    Dynamic Strategy

---
+++ Design Patterns/Behavioral/Strategy/DynamicStrategy/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicStrategy
+{
+    public enum OutputFormat
+    {
+        Markdown,
+        Html
+    }
+
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItem(StringBuilder sb, string item);
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"\t<li>{item}</li>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+    }
+
+    public class MarkdownListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+
+        public void End(StringBuilder sb)
+        {
+        }
+
+        public void Start(StringBuilder sb)
+        {
+        }
+    }
+
+    public class TextProcessor
+    {
+        private StringBuilder sb = new StringBuilder();
+        private IListStrategy listStrategy;
+
+        public void SetOutputFormat(OutputFormat format)
+        {
+            switch (format)
+            {
+                case OutputFormat.Markdown:
+                    listStrategy = new MarkdownListStrategy();
+                    break;
+                case OutputFormat.Html:
+                    listStrategy = new HtmlListStrategy();
+                    break;
+            }
+        }
+
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach (var item in items)
+            {
+                listStrategy.AddListItem(sb, item);
+            }
+            listStrategy.End(sb);
+        }
+
+        public StringBuilder Clear()
+        {
+            return sb.Clear();
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tp = new TextProcessor();
+            tp.SetOutputFormat(OutputFormat.Markdown);
+            tp.AppendList(new[] { "apple", "banana", "lemon" });
+            Console.WriteLine(tp);
+            tp.Clear();
+            tp.SetOutputFormat(OutputFormat.Html);
+            tp.AppendList(new[] { "apple", "banana", "lemon" });
+
+            Console.WriteLine();
+            Console.WriteLine(tp);
+
+        }
+
+    }
+}
